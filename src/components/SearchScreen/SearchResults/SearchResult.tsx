d19a04320735fcda39ade49aed7529ad8aff8009
kohwahskee <hidonbrush@gmail.com>
2023-03-09 18:59:48 -0600

    Items now behave individually, no longer relies on container

--- src/components/SearchScreen/SearchResults/SearchResult.tsx
+++ src/components/SearchScreen/SearchResults/SearchResult.tsx
@@ -1,82 +1,95 @@
-import { SpringValue, animated, useSpringValue, easings } from '@react-spring/web';
-import { useEffect, useRef, useState } from 'react';
+import { animated, useSpringValue } from '@react-spring/web';
+import { useEffect, useRef } from 'react';
 import './style.scss';
 
 interface Props {
+	index: number;
 	setActiveResult: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
 	yPos: number;
 }
 
-export default function SearchResult({ setActiveResult, yPos }: Props) {
+export default function SearchResult({ index, setActiveResult, yPos }: Props) {
 	const searchResultRef = useRef<HTMLDivElement>(null);
 	const parentRectRef = useRef<DOMRect>();
-	const [scale, setScale] = useState(1);
-	const initialCenterPoint = useRef(0);
-	const scaleAnimation = useSpringValue(scale);
+	const initialRef = useRef<DOMRect>();
+	const scrollYAnimation = useSpringValue(yPos);
 
 	useEffect(() => {
 		if (!searchResultRef.current) return;
 		const elRect = searchResultRef.current.getBoundingClientRect();
 		parentRectRef.current =
 			searchResultRef.current.parentElement?.parentElement?.getBoundingClientRect();
-		const elCenterPoint = elRect.top + elRect.height / 2;
-		initialCenterPoint.current = elCenterPoint;
+		initialRef.current = elRect;
 	}, []);
 
 	useEffect(() => {
 		if (!searchResultRef.current || !parentRectRef.current) return;
-		setScale(
-			getScaleFromDistance(
-				searchResultRef.current,
-				yPos,
-				initialCenterPoint.current,
-				parentRectRef.current
-			)
-		);
-	}, [yPos]);
 
-	useEffect(() => {
-		scaleAnimation.start(scale, {
-			config: {
-				mass: 1,
-				tension: 200,
-				friction: 20,
-			},
-		});
-	}, [scale, scaleAnimation]);
+		if (shouldAnimate()) {
+			searchResultRef.current.style.opacity = '1';
+			scrollYAnimation.start(yPos);
+		} else {
+			searchResultRef.current.style.opacity = '0';
+		}
+
+		function shouldAnimate() {
+			if (!searchResultRef.current || !parentRectRef.current || !initialRef.current) return false;
+
+			const elRect = searchResultRef.current.getBoundingClientRect();
+			const parentRect = parentRectRef.current;
+
+			const currentTop = elRect.top;
+			const currentBottom = elRect.bottom;
+			const futureTop = initialRef.current.top + yPos;
+			const futureBottom = initialRef.current.bottom + yPos;
+
+			// If future position OR  current position  is in view
+			// -> Animate
+			return (
+				(currentTop >= parentRect.top && currentBottom <= parentRect.bottom) ||
+				(futureTop >= parentRect.top && futureBottom <= parentRect.bottom)
+			);
+		}
+	}, [scrollYAnimation, yPos]);
 
 	return (
 		<animated.div
 			className='search-result'
-			style={{ transform: scaleAnimation.to((value) => `scale3d(${value},${value},1)`) }}
-			ref={searchResultRef}>
+			style={{
+				transform: scrollYAnimation.to((value) => `translate3d(0,${value}px,0)`),
+				scale: scrollYAnimation.to(() => {
+					if (!searchResultRef.current || !parentRectRef.current) return 1;
+					const distanceFromCenter = getDistanceFromCenter(
+						searchResultRef.current,
+						parentRectRef.current
+					);
+
+					return Math.max(0, Math.min(1, 1 - Math.pow(0.003 * distanceFromCenter, 2)));
+				}),
+				opacity: scrollYAnimation.to(() => {
+					if (!searchResultRef.current || !parentRectRef.current) return 1;
+					const distanceFromCenter = getDistanceFromCenter(
+						searchResultRef.current,
+						parentRectRef.current
+					);
+
+					return Math.max(0, Math.min(1, 1 - Math.pow(0.003 * distanceFromCenter, 1.7)));
+				}),
+			}}
+			ref={searchResultRef}
+			onClick={() => setActiveResult(searchResultRef.current)}>
 			<div className='search-result-content'>
-				<h1 className='search-index-number'>123</h1>
-				<a onClick={() => setActiveResult(searchResultRef.current)}>
-					Lorem ipsum dolor sit amet consectetur adipisicing.
-				</a>
-				<a onClick={() => setActiveResult(searchResultRef.current)}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				</a>
+				<h1 className='search-index-number'>{index}</h1>
+				<a>Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+				<a>Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
 			</div>
 		</animated.div>
 	);
 }
 
-function getScaleFromDistance(
-	el: HTMLElement,
-	yPos: number,
-	initialCenterPoint: number,
-	parentRect: DOMRect
-) {
-	if (!el.parentElement?.parentElement) return 1;
-
+function getDistanceFromCenter(el: HTMLElement, parentRect: DOMRect) {
+	const elRect = el.getBoundingClientRect();
 	const centerPoint = parentRect.top + parentRect.height / 2;
-	const elCenterPoint = initialCenterPoint + yPos;
-	const distanceFromCenter = Math.abs(centerPoint - elCenterPoint);
-
-	if (elCenterPoint < parentRect.top || elCenterPoint > parentRect.bottom) return 0;
-
-	return Math.max(0, Math.min(1, 1 - Math.pow(0.003 * distanceFromCenter, 2)));
-	// return Math.max(0, Math.min(1, 1 - (distanceFromCenter / 1000) * 2));
+	const elCenterPoint = elRect.top + elRect.height / 2;
+	return Math.abs(centerPoint - elCenterPoint);
 }
--- src/components/SearchScreen/SearchScreen.tsx
+++ src/components/SearchScreen/SearchScreen.tsx
@@ -1,39 +1,15 @@
 import './style.scss';
 import SearchResult from './SearchResults/SearchResult';
-import { animated, useSpring, useSpringValue } from '@react-spring/web';
+import { useSpringValue } from '@react-spring/web';
 import { useEffect, useRef, useState, WheelEvent } from 'react';
 
-// TODO:
-// 1. Try animating every single item instead of the container
-//  - If items are out of view, they will not be animated (reduce performance cost)
-
 export default function SearchScreen() {
-	// const SCROLL_AMOUNT = 50;
+	const SCROLL_AMOUNT = 100;
 	const [yPos, setYPos] = useState(0);
-	const resultsRef = useRef<HTMLDivElement[]>(null);
 	const resultsListRef = useRef<HTMLDivElement[]>([]);
-	const currentResultIndex = useRef(0);
 	const [activeResult, setActiveResult] = useState<HTMLElement | null>(null);
-
 	const scrollYAnimation = useSpringValue(yPos);
-
 	const containerRef = useRef<HTMLDivElement>(null);
-
-	const onScrollHandler = (e: WheelEvent) => {
-		const nextItem = e.currentTarget.children[0].children;
-		// setYPos((prev) => {
-		// 	return e.deltaY < 0 ? prev + SCROLL_AMOUNT : prev - SCROLL_AMOUNT;
-		// });
-		if (e.deltaY < 0) {
-			currentResultIndex.current--;
-			setActiveResult(resultsListRef.current[currentResultIndex.current]);
-		} else {
-			currentResultIndex.current++;
-			setActiveResult(resultsListRef.current[currentResultIndex.current]);
-		}
-
-		// setActiveResult(null);
-	};
 
 	useEffect(() => {
 		resultsListRef.current = Array.from(document.querySelectorAll('.search-result'));
@@ -46,37 +22,39 @@ export default function SearchScreen() {
 				easing: (t) => t,
 			},
 		});
-	}, [yPos]);
+	}, [scrollYAnimation, yPos]);
 
 	useEffect(() => {
 		if (!activeResult) return;
-		const distanceFromCenter = getDistanceFromCenter(activeResult);
-		setYPos(distanceFromCenter);
+		setYPos((prev) => getDistanceFromCenter(activeResult) + prev);
+		resultsListRef.current.forEach((el) => {
+			el.classList.remove('active-result');
+		});
+		activeResult.classList.add('active-result');
 	}, [activeResult]);
 
-	function getDistanceFromCenter(el: HTMLElement): number {
-		if (!el.parentElement) return 0;
-
-		const parentEl = el.parentElement;
-		const parentRect = parentEl.getBoundingClientRect();
-		const elRect = el.getBoundingClientRect();
-		const centerPoint = parentRect.top + parentRect.height / 2;
-		const elCenterPoint = elRect.top + elRect.height / 2;
-
-		return centerPoint - elCenterPoint;
+	function onScrollHandler(e: WheelEvent) {
+		scrollBy(e.deltaY < 0 ? SCROLL_AMOUNT : -SCROLL_AMOUNT);
 	}
 
-	function scrollToItem(el: HTMLElement) {
-		setYPos(getDistanceFromCenter(el));
+	function mouseDownHandler() {
+		document.addEventListener('mousemove', mouseMoveHandler);
+		document.addEventListener('mouseup', mouseUpHandler);
 	}
 
-	function mouseDownHandler(e: React.MouseEvent) {
-		e.currentTarget?.addEventListener('mousemove', mouseMoveHandler);
+	function mouseMoveHandler(e: MouseEvent) {
+		scrollBy(e.movementY);
 	}
-	function mouseMoveHandler(e: Event) {
-		setYPos((prev) => {
-			return prev + (e as MouseEvent).movementY;
-		});
+
+	function mouseUpHandler() {
+		console.log('mouse up');
+		document.removeEventListener('mousemove', mouseMoveHandler);
+		document.removeEventListener('mouseup', mouseUpHandler);
+	}
+
+	function scrollBy(amount: number) {
+		setYPos((prev) => prev + amount);
+		setActiveResult(null);
 	}
 
 	return (
@@ -84,22 +62,35 @@ export default function SearchScreen() {
 			onMouseDown={mouseDownHandler}
 			onWheel={onScrollHandler}
 			className='result-container'>
-			<div className='indicator-line left-line' />
-			<div className='indicator-line right-line' />
-			<animated.div
-				style={{ transform: scrollYAnimation.to((value) => `translate3d(0,${value}px, 0)`) }}
+			<hr className={`indicator-line left-line ${!activeResult ? 'selecting' : ''}`} />
+			<hr className={`indicator-line right-line ${!activeResult ? 'selecting' : ''}`} />
+			<div
 				ref={containerRef}
 				className='search-results-wrapper'>
-				{[...Array(200)].map((item, i) => {
+				{[...Array(40)].map((item, i) => {
 					return (
 						<SearchResult
+							index={i}
 							key={i}
 							setActiveResult={setActiveResult}
 							yPos={yPos}
 						/>
 					);
 				})}
-			</animated.div>
+			</div>
 		</div>
 	);
+}
+
+function getDistanceFromCenter(el: HTMLElement): number {
+	if (!el.parentElement) return 0;
+
+	const parentEl = el.parentElement;
+	const parentRect = parentEl.getBoundingClientRect();
+	const elRect = el.getBoundingClientRect();
+	const centerPoint = parentRect.top + parentRect.height / 2;
+	const elCenterPoint = elRect.top + elRect.height / 2;
+
+	// debugger;
+	return centerPoint - elCenterPoint;
 }
